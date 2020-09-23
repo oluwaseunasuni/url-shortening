b1958b9336605779918e8265e50b194733404818
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-09-23 01:10:15 +0100

    Installed SASS loader

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,18 +1,20 @@
 
 <template>
   <div class="hero">
-    <div>
-      <h1>
-        More than just shorter links
-      </h1>
+    <div class="hero__content">
+      <div class="left">
+        <h1>
+          More than just shorter links
+        </h1>
         <p>
           Build your brand’s recognition and get detailed insights
           on how your links are performing.
         </p>
         <button>Get Started</button>
-    </div>
-    <div>
-      <img src="@/assets/images/illustration-working.svg" alt="woking illustration">
+      </div>
+      <div class="right">
+        <img src="@/assets/images/illustration-working.svg" alt="woking illustration">
+      </div>
     </div>
   </div>
 </template>
@@ -24,5 +26,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.hero__content {
+  display: flex;
+}
 
 </style>
