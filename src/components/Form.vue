2a3479bee65bfa6e4a84db64998d94c4e7003a7a
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-09-28 02:03:33 +0100

    created cards

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    Shorten It!
+  <div class="container">
+    <form action="">
+      <div>
+        <input type="text" name="" id="">
+        <button>
+          Shorten It!
+        </button>
+      </div>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+form {
+  background-image: url('../assets/images/bg-shorten-desktop.svg');
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,29 +1,87 @@
 <template>
-  <div>
-  Advanced Statistics
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Advanced Statistics
+      </h1>
+      <h2 class="subtitle">
+        Track how your links are performing across the web with our
+        advanced statistics dashboard.
+      </h2>
 
-  Track how your links are performing across the web with our
-  advanced statistics dashboard.
+      <div class="columns">
 
-  Brand Recognition
+        <div class="column card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="../assets/images/icon-brand-recognition.svg" alt="">
+                </figure>
+              </div>
+            </div>
+            <div class="content">
+              <p class="card-header-title">
+                Brand Recognition
+              </p>
+              <p>
+                Boost your brand recognition with each click. Generic links don’t
+                mean a thing. Branded links help instil confidence in your content.
+              </p>
+            </div>
+          </div>
+        </div>
 
-  Boost your brand recognition with each click. Generic links don’t
-  mean a thing. Branded links help instil confidence in your content.
+        <div class="column card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="../assets/images/icon-detailed-records.svg" alt="">
+                </figure>
+              </div>
+            </div>
+            <div class="content">
+              <p class="card-header-title">
+                Detailed Records
+              </p>
+              <p>
+                Gain insights into who is clicking your links. Knowing when and where
+                people engage with your content helps inform better decisions.
+              </p>
+            </div>
+          </div>
+        </div>
 
-  Detailed Records
-
-  Gain insights into who is clicking your links. Knowing when and where
-  people engage with your content helps inform better decisions.
-
-  Fully Customizable
-
-  Improve brand awareness and content discoverability through customizable
-  links, supercharging audience engagement.
-
-  <!-- CTA Just before the footer -->
-  Boost your links today
-
-  Get Started
-  </div>
-
+        <div class="column card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="../assets/images/icon-fully-customizable.svg" alt="">
+                </figure>
+              </div>
+            </div>
+            <div class="content">
+              <p class="card-header-title">
+                Fully Customizable
+              </p>
+              <p>
+                Improve brand awareness and content discoverability through customizable
+                links, supercharging audience engagement.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>
+          Boost your links today
+        </h2>
+        <button>
+          Get Started
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
